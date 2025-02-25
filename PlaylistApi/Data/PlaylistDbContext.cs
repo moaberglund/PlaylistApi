364cb0cdc87effd72a056f0a88e75f258272874f
moaberglund <moa.21@hotmail.com>
2025-02-25 13:40:00 +0100

    reform dbcontext

--- PlaylistApi/Data/PlaylistDbContext.cs
+++ PlaylistApi/Data/PlaylistDbContext.cs
@@ -3,8 +3,12 @@ using PlaylistApi.Models;
 
 namespace PlaylistApi.Data
 {
-    public class PlaylistDbContext(DbContextOptions<PlaylistDbContext> options) : DbContext(options)
+    public class PlaylistContext : DbContext
     {
+        public PlaylistContext(DbContextOptions<PlaylistContext> options) : base(options) { }
+
         public DbSet<Track> Tracks { get; set; }
+
     }
+
 }
