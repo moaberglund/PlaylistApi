4cd374617c9162a30d2be5ab19931866c9562df6
moaberglund <moa.21@hotmail.com>
2025-02-25 13:25:04 +0100

    updated model with request fields

--- PlaylistApi/Track.cs
+++ PlaylistApi/Track.cs
@@ -1,15 +1,21 @@
-﻿namespace PlaylistApi
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlaylistApi
 {
     public class Track
     {
         public int Id { get; set; }
 
+        [Required]
         public string? Title { get; set; }
 
+        [Required]
         public string? Artist { get; set; }
 
+        [Required]
         public int? Length { get; set; }
 
+        [Required]
         public string? Album { get; set; }
 
         public string? Category { get; set; }
