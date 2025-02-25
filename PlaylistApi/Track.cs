1d6d8644ab2f48917fc8655f1d652b73d27fdb57
moaberglund <moa.21@hotmail.com>
2025-02-25 13:12:31 +0100

    Add Track class to PlaylistApi namespace
    Introduces a new `Track` class with properties for
    `Id`, `Title`, `Artist`, `Length`, `Album`, and
    `Category` to represent music tracks in the application.

---
+++ PlaylistApi/Track.cs
@@ -0,0 +1,17 @@
+﻿namespace PlaylistApi
+{
+    public class Track
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Artist { get; set; }
+
+        public int? Length { get; set; }
+
+        public string? Album { get; set; }
+
+        public string? Category { get; set; }
+    }
+}
