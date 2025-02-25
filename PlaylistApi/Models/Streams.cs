94507850f72c91559c105d25b9cf297ee85cb188
moaberglund <moa.21@hotmail.com>
2025-02-25 16:18:34 +0100

    Add Streams model to PlaylistApi.Models namespace

---
+++ PlaylistApi/Models/Streams.cs
@@ -0,0 +1,17 @@
+﻿namespace PlaylistApi.Models
+{
+    public class Streams
+    {
+        public int Id { get; set; }
+
+        public int? SpotifyStreams { get; set; }
+
+        public int? AppleMusicStreams { get; set; }
+
+        public int? AmazonMusicStreams { get; set; }
+
+
+        public int TrackId { get; set; }
+
+    }
+}
