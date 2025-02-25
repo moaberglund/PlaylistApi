5aabe31579a7128f87847c733a9f14a42aa8f7b7
moaberglund <moa.21@hotmail.com>
2025-02-25 13:33:45 +0100

    Add PlaylistDbContext for Track entity management

---
+++ PlaylistApi/Data/PlaylistDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlaylistApi.Models;
+
+namespace PlaylistApi.Data
+{
+    public class PlaylistDbContext(DbContextOptions<PlaylistDbContext> options) : DbContext(options)
+    {
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
