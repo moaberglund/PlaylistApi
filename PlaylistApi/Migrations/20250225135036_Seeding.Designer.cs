404382f4a6db1cdb5d540b1880e9221781225f70
moaberglund <moa.21@hotmail.com>
2025-02-25 14:51:28 +0100

    Add migration for seeding initial track data

--- PlaylistApi/Migrations/20250225135036_Seeding.Designer.cs
+++ PlaylistApi/Migrations/20250225135036_Seeding.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlaylistApi.Data;
 
@@ -10,9 +11,11 @@ using PlaylistApi.Data;
 namespace PlaylistApi.Migrations
 {
     [DbContext(typeof(PlaylistContext))]
-    partial class PlaylistContextModelSnapshot : ModelSnapshot
+    [Migration("20250225135036_Seeding")]
+    partial class Seeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PlaylistApi/Migrations/20250225135036_Seeding.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PlaylistApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tracks",
+                columns: new[] { "Id", "Album", "Artist", "Category", "Length", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Currents", "Tame Impala", "Indie", 216, "The Less I Know The Better" },
+                    { 2, "Rumours", "Fleetwood Mac", "Rock", 256, "Dreams" },
+                    { 3, "Arrival", "ABBA", "Pop", 227, "Dancing Queen" },
+                    { 4, "A Night at the Opera", "Queen", "Rock", 354, "Bohemian Rhapsody" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tracks",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tracks",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tracks",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tracks",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
