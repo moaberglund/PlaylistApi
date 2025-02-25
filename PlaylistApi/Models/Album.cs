2c873da66c86efc45caef55b9f92354d72e26fdb
moaberglund <moa.21@hotmail.com>
2025-02-25 17:35:36 +0100

    Add Album class to PlaylistApi.Models namespace

---
+++ PlaylistApi/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿
+namespace PlaylistApi.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public required string AlbumTitle { get; set; }
+
+        public int? Year { get; set; }
+
+        public string? Studio { get; set; }
+
+    }
+}
