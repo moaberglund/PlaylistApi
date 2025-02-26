8e8f568533eb2ad84918532e0ea9a8992ec32710
moaberglund <moa.21@hotmail.com>
2025-02-26 09:48:20 +0100

    Add Genre class to PlaylistApi.Models namespace

---
+++ PlaylistApi/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PlaylistApi.Models
+{
+    public class Genre
+    {
+
+        public int Id { get; set; }
+        public required string GenreName { get; set; }
+
+        [JsonIgnore]
+        public List<Track>? Tracks { get; set; }
+    }
+}
