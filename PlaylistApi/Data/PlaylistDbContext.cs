ec3f45bf9d908f17f34bda00bb1feaa842da15d7
moaberglund <moa.21@hotmail.com>
2025-02-26 09:48:08 +0100

    Remove Category property from Track objects

--- PlaylistApi/Data/PlaylistDbContext.cs
+++ PlaylistApi/Data/PlaylistDbContext.cs
@@ -21,32 +21,28 @@ namespace PlaylistApi.Data
                     Id = 1,
                     Title = "The Less I Know The Better",
                     Artist = "Tame Impala",
-                    Length = 216,
-                    Category = "Indie"
+                    Length = 216
                 },
                 new Track
                 {
                     Id = 2,
                     Title = "Dreams",
                     Artist = "Fleetwood Mac",
-                    Length = 256,
-                    Category = "Rock"
+                    Length = 256
                 },
                 new Track
                 {
                     Id = 3,
                     Title = "Dancing Queen",
                     Artist = "ABBA",
-                    Length = 227,
-                    Category = "Pop"
+                    Length = 227
                 },
                 new Track
                 {
                     Id = 4,
                     Title = "Bohemian Rhapsody",
                     Artist = "Queen",
-                    Length = 354,
-                    Category = "Rock"
+                    Length = 354
                 }
                 );
         }
