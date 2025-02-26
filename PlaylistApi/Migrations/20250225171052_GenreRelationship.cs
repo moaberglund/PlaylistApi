22efef3336dc3418b6b9a5c4335718ee4468a82a
moaberglund <moa.21@hotmail.com>
2025-02-26 09:48:34 +0100

    Add many-to-many relationship between Genre and Track

---
+++ PlaylistApi/Migrations/20250225171052_GenreRelationship.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlaylistApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class GenreRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Tracks");
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenreName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GenreTrack",
+                columns: table => new
+                {
+                    GenresId = table.Column<int>(type: "int", nullable: false),
+                    TracksId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GenreTrack", x => new { x.GenresId, x.TracksId });
+                    table.ForeignKey(
+                        name: "FK_GenreTrack_Genre_GenresId",
+                        column: x => x.GenresId,
+                        principalTable: "Genre",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GenreTrack_Tracks_TracksId",
+                        column: x => x.TracksId,
+                        principalTable: "Tracks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GenreTrack_TracksId",
+                table: "GenreTrack",
+                column: "TracksId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GenreTrack");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Tracks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Tracks",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Category",
+                value: "Indie");
+
+            migrationBuilder.UpdateData(
+                table: "Tracks",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Category",
+                value: "Rock");
+
+            migrationBuilder.UpdateData(
+                table: "Tracks",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Category",
+                value: "Pop");
+
+            migrationBuilder.UpdateData(
+                table: "Tracks",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Category",
+                value: "Rock");
+        }
+    }
+}
